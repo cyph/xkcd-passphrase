dba461ead4e89f6074987145c1f238f8120c459b
Ryan Lester <hacker@linux.com>
2017-09-22 19:25:58 -0700

    allow custom word list via command line

--- cli.js
+++ cli.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 
-require('xkcd-passphrase').generate(parseInt(process.argv[2], 10)).then(s => console.log(s));
+require('./dist/xkcd-passphrase').generate(
+	parseInt(process.argv[2], 10),
+	process.argv[3] && require('fs').readFileSync(process.argv[3]).toString().split('\n')
+).then(s => console.log(s));
