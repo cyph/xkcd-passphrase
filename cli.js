c4a43fe0655d458744f997a2486e9916eb5bc5f1
Ryan Lester <hacker@linux.com>
2022-06-30 22:05:00 -0400

    consolidated into pqcrypto.js

--- cli.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-require('./dist/xkcd-passphrase').generate(
-	parseInt(process.argv[2], 10),
-	process.argv[3] && require('fs').readFileSync(process.argv[3]).toString().trim().split('\n')
-).then(s => console.log(s));
